e8e9b6c3024533ecdf9a195292b09a767a38f147
Shubham <35026297+porcelainruler@users.noreply.github.com>
2018-07-15 22:09:09 +0530

    Functionality(Initial-Phase)

--- script.js
+++ script.js
@@ -4,8 +4,36 @@ window.onload=function(){
     var start=document.getElementById("start");
     var pause=document.getElementById("pause");
     var refresh=document.getElementById("refresh");
+    var breakup=document.getElementById("break-up");
+    var breaklabel=document.getElementById("break-label");
+    var breakdown=document.getElementById("break-down");
+    var strengthup=document.getElementById("strength-up");
+    var strengthlabel=document.getElementById("strength-label");
+    var strengthdown=document.getElementById("strength-down");
 
     var flag=true;
+    display.innerText="25:00";
+
+    breaklabel.innerText="5";
+    strengthlabel.innerText="25";
+
+    breakup.onclick=function(){
+        breaklabel.innerText=parseInt(breaklabel.innerText)+1;
+    }
+
+    breakdown.onclick=function(){
+        breaklabel.innerText=parseInt(breaklabel.innerText)-1;
+    }
+
+    strengthup.onclick=function(){
+        strengthlabel.innerText=parseInt(strengthlabel.innerText)+1;
+        display.innerText=parseInt(strengthlabel.innerText) + ":00";
+    }
+
+    strengthdown.onclick=function(){
+        strengthlabel.innerText=parseInt(strengthlabel.innerText)-1;
+        display.innerText=parseInt(strengthlabel.innerText) + ":00";
+    }
 
     refresh.onclick=function(){
         display.innerText="25:00";
