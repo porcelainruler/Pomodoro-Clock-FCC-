de35bf801ca46e4832d89dc8764a37ac5763a53c
Shubham <35026297+porcelainruler@users.noreply.github.com>
2018-07-15 14:20:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-var clock=document.getElementById("clock");
+window.onload=function(){
+    var clock=document.getElementById("clock");
+    var display=document.getElementById("display");
+    var start=document.getElementById("start");
+    var pause=document.getElementById("pause");
+    var refresh=document.getElementById("refresh");
 
+    var flag=true;
+
+    refresh.onclick=function(){
+        display.innerText="25:00";
+    }
+
+    start.onclick=function(){
+
+        if(flag){
+            display.innerText="25:00";
+            flag=false;
+        }
+
+    }
+
+}
